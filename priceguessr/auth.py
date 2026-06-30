677ae27c1bf8de69d5f06e01b78e8312fe7fe2a9
Nikoloz-Lomtadze <nikoloz.lomtadze.10@gmail.com>
2026-06-30 21:22:00 +0400

    changed readme and auth, added hash

--- priceguessr/auth.py
+++ priceguessr/auth.py
@@ -1,38 +1,50 @@
+from argon2 import PasswordHasher
+from argon2.exceptions import InvalidHashError, VerifyMismatchError
 from priceguessr.models import Users
-## კოდი აუტემფიკაციის log in  Sign up
+
+#აუტენფიკაციის და პაროლის ჰაშის კოდი
+
+password_hasher = PasswordHasher() # არგონ2-ის  ჰაშერის ობიექტი
+#სხვათაშორის ეს ჰაშერი და კოდები youtube-ში ვნახე :)
 
 class AuthManager:
-    def __init__(self, database): # ბაზის ობიექტი
+    def __init__(self, database):
         self.database = database
-
-    def signup(self, username, password): #Sign up
+    def signup(self, username, password): #sign up
+        password_hash = password_hasher.hash(password) #ვჰაშავთ პაროლს
         conn = self.database.connect()
-        try:  # ამატებს ახალ მომხმარებელს
-            cursor = conn.cursor()
-            prompt = """
-            INSERT INTO users (username, password) VALUES (?, ?)
-            """
-            cursor.execute(prompt, (username, password))
-            conn.commit()
-            return Users(cursor.lastrowid, username) # გვიბრუნებს მომხმარებლის ობიექტს id,usernmae
-            # lastrowid ახლად დამატებული row-ს  აიდი
-        finally:
-            conn.close()
 
-    def login(self, username, password): #login
+
+        try:
+            cursor = conn.cursor()
+
+            prompt = """INSERT INTO users (username, password) VALUES (?, ?)"""
+            cursor.execute(prompt, (username, password_hash)) # ვავსებთ მომხმარებლის მონაცემებს
+            conn.commit()
+
+            return Users(cursor.lastrowid, username) #მოგვაქვს user ობიექტი აიდით და სახელით
+        finally:
+            conn.close() # ვხურავთ კავშირს
+
+    def login(self, username, password):
+
+
         conn = self.database.connect()
         try:
             cursor = conn.cursor()
-            #ვეძებთ ცხრილში რომ მომხმარებელი და პაროლი ერთი იყოს
-            prompt = """    
-            SELECT * FROM users
-            WHERE username = ? AND password = ?
-            """ 
-            cursor.execute(prompt, (username, password))
-            row = cursor.fetchone() #
+            prompt = """SELECT * FROM users WHERE username = ?"""
+            cursor.execute(prompt, (username,))
+            row = cursor.fetchone() # მოგვაქვს მომხამრებელი
         finally:
             conn.close()
-
         if row is None:
             return None
-        return Users(row["id"], row["username"]) # აქაც ვაბრუნებთ აიდის და მომხმარებელს
+        try:
+        
+
+            password_hasher.verify(row["password"], password) # ვამოწმებთ ერთია თუ არა
+        
+        except (VerifyMismatchError, InvalidHashError): # თუ არა მაშინ აბრუნებს ერორს
+            return None
+
+        return Users(row["id"], row["username"]) #გვიბრუნებს მომხმარებლის აიდის და სახელს
